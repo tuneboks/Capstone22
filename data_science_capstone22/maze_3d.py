3647ee9c10e370c8d16e754b4f75a72d63e58dd9
tuneboks <65516753+tuneboks@users.noreply.github.com>
2022-11-15 18:38:34 -0600

    Update maze_3d.py

--- data_science_capstone22/maze_3d.py
+++ data_science_capstone22/maze_3d.py
@@ -1,1 +1,65 @@
 
+import logging
+from enum import Enum, IntEnum
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Cell(IntEnum):
+    EMPTY = 0  # indicates empty cell where the agent can move to
+    OCCUPIED = 1  # indicates cell which contains a wall and cannot be entered
+    CURRENT = 2  # indicates current cell of the agent
+
+
+class Action(IntEnum):
+    MOVE_LEFT = 0
+    MOVE_RIGHT = 1
+    MOVE_UP = 2
+    MOVE_DOWN = 3
+    MOVE_FORWARD = 4
+    MOVE_BACK = 5
+
+
+class Render(Enum):
+    NOTHING = 0
+    TRAINING = 1
+    MOVES = 2
+
+
+class Status(Enum):
+    WIN = 0
+    LOSE = 1
+    PLAYING = 2
+
+class Maze:
+    actions = [Action.MOVE_LEFT, Action.MOVE_RIGHT, Action.MOVE_UP, Action.MOVE_DOWN, Action.FORWARD, Action.BACK]  # all possible actions
+    reward_exit = 10.0  # reward for reaching the exit cell
+    penalty_move = -0.05  # penalty for a move which did not result in finding the exit cell
+    penalty_visited = -0.25  # penalty for returning to a cell which was visited earlier
+    penalty_impossible_move = -0.75  # penalty for trying to enter an occupied cell or moving out of the maze
+
+    def __init__(self, maze, start_cell=(0, 0, 0), exit_cell=None):
+        self.maze = maze
+
+        self.__minimum_reward = -0.5 * self.maze.size  # stop game if accumulated reward is below this threshold
+
+        nrows, ncols = self.maze.shape
+        self.cells = [(col, row) for col in range(ncols) for row in range(nrows)]
+        self.empty = [(col, row) for col in range(ncols) for row in range(nrows) if self.maze[row, col] == Cell.EMPTY]
+        self.__exit_cell = (ncols - 1, nrows - 1) if exit_cell is None else exit_cell
+        self.empty.remove(self.__exit_cell)
+
+        # Check for impossible maze layout
+        if self.__exit_cell not in self.cells:
+            raise Exception("Error: exit cell at {} is not inside maze".format(self.__exit_cell))
+        if self.maze[self.__exit_cell[::-1]] == Cell.OCCUPIED:
+            raise Exception("Error: exit cell at {} is not free".format(self.__exit_cell))
+
+        # Variables for rendering using Matplotlib
+        self.__render = Render.NOTHING  # what to render
+        self.__ax1 = None  # axes for rendering the moves
+        self.__ax2 = None  # axes for rendering the best action per cell
+        self.__ax3 = None
+
+        self.reset(start_cell)
